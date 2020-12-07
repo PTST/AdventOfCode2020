19865de608e1300e89db82f9cdf9e8a829130c8d
Patrick Toft Steffensen <ptf@econta.dk>
2020-12-07 09:45:39 +0100

    Day5, 6, & 7

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -14,9 +14,20 @@ namespace AdventOfCode
 
             //var result = Day3.Part1();
             //var result = Day3.Part2();
-            
+
             //var result = Day4.Part1();
-            var result = Day4.Part2();
+            //var result = Day4.Part2();
+
+            //var result = Day5.Part1();
+            //var result = Day5.Part2();
+
+            //var result = Day6.Part1();
+            //var result = Day6.Part2();
+
+            //var result = Day7.Part1();
+            var result = Day7.Part2();
+
+
 
             Console.WriteLine(result);
         }
